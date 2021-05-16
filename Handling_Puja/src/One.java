186bf31bfac0cb256753159ac4b31898ba06abad
PujaJyoti <69318249+PujaJyoti@users.noreply.github.com>
2021-05-16 13:34:38 +0530

    Add files via upload

---
+++ Handling_Puja/src/One.java
@@ -0,0 +1,21 @@
+import Login.Handling_Login;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*Handling_Login hd =new Handling_Login();
+hd.enterpwd();
+hd.enteruid();
+hd.clickssubmit();*/
+		//System.out.println("Print the messages");
+		System.out.println("The first data is:"+ args[0]);
+		System.out.println("The first data is:"+ args[1]);
+		System.out.println("The first data is:"+ args[2]);
+		System.out.println("The first data is:"+ args[3]);
+	}
+/*static{
+	System.out.println("Preexecution block");
+}*/
+	
+}
