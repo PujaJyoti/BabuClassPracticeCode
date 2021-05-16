bec3d05003be55b7183053c12261ff6d107eee42
PujaJyoti <69318249+PujaJyoti@users.noreply.github.com>
2021-05-16 13:30:38 +0530

    Add files via upload

---
+++ Handling_ExternalProjects/src/Login/Handling_Login.java
@@ -0,0 +1,19 @@
+package Login;
+
+public class Handling_Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void enteruid(){
+		
+	}
+	public void enterpwd(){
+		
+	}
+	public void clickssubmit(){
+		
+	}
+
+}
